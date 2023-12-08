74adb1aa44b9b41274214fdc25100468c6cc8eb6
Đặng Minh Đức <ducware@gmail.com>
2023-12-08 14:52:52 +0700

    update

--- Date.API/Program.cs
+++ Date.API/Program.cs
@@ -6,10 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-builder.Services.AddHealthChecks();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
